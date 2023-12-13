237fffe0c3fccce7a4bffbc8245481b92cd59d2c
AnshikaArora207 <124586347+AnshikaArora207@users.noreply.github.com>
2023-12-13 22:47:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,17 +7,48 @@ const firebaseConfig = {
   messagingSenderId: "897422691159",
   appId: "1:897422691159:web:7525ab154631306070cc12"
 };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database().ref("comments");
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the Firestore database service
+const db = firebase.firestore();
+
+// Reference to the 'comments' collection
+const commentsRef = db.collection("comments");
+
+// Function to display comments
+function displayComments() {
+  commentsRef.get().then((querySnapshot) => {
+    const commentsContainer = document.querySelector('.container.cont'); // Target the container where comments will be displayed
+
+    querySnapshot.forEach((doc) => {
+      const commentData = doc.data();
+      const commentElement = document.createElement('div');
+      commentElement.innerHTML = `<div class="bg-white rounded-lg flex flex-col shadow-cyan-400/80 shadow-xl my-4 text-black p-2">
+        <p><strong>Name : </strong> ${commentData.name}</p>
+        <p><strong>Comment : </strong> ${commentData.comment}</p>
+        </div>
+      `;
+      commentsContainer.appendChild(commentElement);
+    });
+  }).catch((error) => {
+    console.error("Error getting comments: ", error);
+  });
+}
+
+// Call the displayComments function to fetch and display comments when the page loads
+window.addEventListener('load', displayComments);
+
+// Form submission and saving comment function (if needed)
 document.getElementById('comments').addEventListener('submit', submitForm);
 
 function submitForm(e) {
   e.preventDefault();
   var name = getElementVal('name');
   var comment = getElementVal('comment');
-  console.log(name, comment);
   saveMessage(name, comment);
+  document.getElementById('comments').reset(); // Reset form after submission
 }
 
 const getElementVal = (id) => {
@@ -25,22 +56,14 @@ const getElementVal = (id) => {
 }
 
 const saveMessage = (name, comment) => {
-  var key = db.push();
-  key.set({
+  commentsRef.add({
     name: name,
     comment: comment
   })
+  .then(() => {
+    console.log("Comment added successfully!");
+  })
+  .catch((error) => {
+    console.error("Error adding comment: ", error);
+  });
 }
-
-
-
-// const form = document.getElementById('comments');
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     db.collection('comments').add({
-//        name: form.name.value,
-//        comment: form.comment.value
-//     });
-//     form.name.value = '';
-//     form.comment.value = '';
-// });
